500a4f60890a8d45f39761a29baadd9549767f2e
Victor Hugo <abjurandam@gmail.com>
2024-06-02 15:55:42 -0300

    Finish HTTP project: print IP of players

--- async/async.ino
+++ async/async.ino
@@ -1,10 +1,3 @@
-//
-// A simple server implementation showing how to:
-//  * serve static messages
-//  * read GET and POST parameters
-//  * handle missing pages / 404s
-//
-
 #include <Arduino.h>
 #ifdef ESP32
 #include <WiFi.h>
@@ -14,6 +7,7 @@
 #include <ESPAsyncTCP.h>
 #endif
 #include <ESPAsyncWebServer.h>
+#include <vector>
 
 AsyncWebServer server(80);
 
@@ -22,233 +16,258 @@ const char* password = "senhaforte33";
 
 const char* PARAM_MESSAGE = "message";
 
+std::vector<String> connectedIPs;
+
 void notFound(AsyncWebServerRequest *request) {
-	request->send(404, "text/plain", "Not found");
+    request->send(404, "text/plain", "Not found");
+}
+
+void updateConnectedIPs(AsyncWebServerRequest *request) {
+    String clientIP = request->client()->remoteIP().toString();
+    if (std::find(connectedIPs.begin(), connectedIPs.end(), clientIP) == connectedIPs.end()) {
+        connectedIPs.push_back(clientIP);
+    }
 }
 
 void setup() {
+    Serial.begin(115200);
+    WiFi.mode(WIFI_STA);
+    WiFi.begin(ssid, password);
+    if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+        Serial.printf("WiFi Failed!\n");
+        return;
+    }
 
-	Serial.begin(115200);
-	WiFi.mode(WIFI_STA);
-	WiFi.begin(ssid, password);
-	if (WiFi.waitForConnectResult() != WL_CONNECTED) {
-		Serial.printf("WiFi Failed!\n");
-		return;
-	}
+    Serial.print("IP Address: ");
+    Serial.println(WiFi.localIP());
 
-	Serial.print("IP Address: ");
-	Serial.println(WiFi.localIP());
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+        updateConnectedIPs(request);
 
-	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-		request->send(200, "text/html", R"rawliteral(
-		<!DOCTYPE html>
-		<html>
-		<head>
-		<meta charset="utf-8" />
-		<title>Gamedev Canvas Workshop - lesson 10: finishing up</title>
-		<style>* { padding: 0; margin: 0; } canvas { background: #eee; display: block; margin: 0 auto; }</style>
-		</head>
-		<body>
+        String html = R"rawliteral(
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta charset="utf-8" />
+        <title>Gamedev Canvas Workshop - lesson 10: finishing up</title>
+        <style>* { padding: 0; margin: 0; } canvas { background: #eee; display: block; margin: 0 auto; }</style>
+        </head>
+        <body>
 
-		<canvas id="myCanvas" width="480" height="320"></canvas>
+        <canvas id="myCanvas" width="480" height="320"></canvas>
 
-		<script>
-		var canvas = document.getElementById("myCanvas");
-		var ctx = canvas.getContext("2d");
-		var ballRadius = 10;
-		var x = canvas.width/2;
-		var y = canvas.height-30;
-		var dx = 2;
-		var dy = -2;
-		var paddleHeight = 10;
-		var paddleWidth = 75;
-		var paddleX = (canvas.width-paddleWidth)/2;
-		var rightPressed = false;
-		var leftPressed = false;
-		var brickRowCount = 5;
-		var brickColumnCount = 3;
-		var brickWidth = 75;
-		var brickHeight = 20;
-		var brickPadding = 10;
-		var brickOffsetTop = 30;
-		var brickOffsetLeft = 30;
-		var score = 0;
-		var lives = 3;
+        <h2>Connected IPs:</h2>
+        <ul id="ipList">
+        )rawliteral";
 
-		var bricks = [];
-		for(var c=0; c<brickColumnCount; c++) {
-		bricks[c] = [];
-		for(var r=0; r<brickRowCount; r++) {
-		bricks[c][r] = { x: 0, y: 0, status: 1 };
-		}
-		}
+        for (const String& ip : connectedIPs) {
+            html += "<li>" + ip + "</li>";
+        }
 
-		document.addEventListener("keydown", keyDownHandler, false);
-		document.addEventListener("keyup", keyUpHandler, false);
-		document.addEventListener("mousemove", mouseMoveHandler, false);
+        html += R"rawliteral(
+        </ul>
 
-		function keyDownHandler(e) {
-		if(e.code  == "ArrowRight") {
-		rightPressed = true;
-		}
-		else if(e.code == 'ArrowLeft') {
-		leftPressed = true;
-		}
-		}
-		function keyUpHandler(e) {
-		if(e.code == 'ArrowRight') {
-		rightPressed = false;
-		}
-		else if(e.code == 'ArrowLeft') {
-		leftPressed = false;
-		}
-		}
-		function mouseMoveHandler(e) {
-		var relativeX = e.clientX - canvas.offsetLeft;
-		if(relativeX > 0 && relativeX < canvas.width) {
-		paddleX = relativeX - paddleWidth/2;
-		}
-		}
-		function collisionDetection() {
-		for(var c=0; c<brickColumnCount; c++) {
-		for(var r=0; r<brickRowCount; r++) {
-		var b = bricks[c][r];
-		if(b.status == 1) {
-		if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
-		dy = -dy;
-		b.status = 0;
-		score++;
-		if(score == brickRowCount*brickColumnCount) {
-		alert("YOU WIN, CONGRATS!");
-		document.location.reload();
-		}
-		}
-		}
-		}
-		}
-		}
+        <script>
+        var canvas = document.getElementById("myCanvas");
+        var ctx = canvas.getContext("2d");
+        var ballRadius = 10;
+        var x = canvas.width/2;
+        var y = canvas.height-30;
+        var dx = 2;
+        var dy = -2;
+        var paddleHeight = 10;
+        var paddleWidth = 75;
+        var paddleX = (canvas.width-paddleWidth)/2;
+        var rightPressed = false;
+        var leftPressed = false;
+        var brickRowCount = 5;
+        var brickColumnCount = 3;
+        var brickWidth = 75;
+        var brickHeight = 20;
+        var brickPadding = 10;
+        var brickOffsetTop = 30;
+        var brickOffsetLeft = 30;
+        var score = 0;
+        var lives = 3;
 
-		function drawBall() {
-		ctx.beginPath();
-		ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-		ctx.fillStyle = "#0095DD";
-		ctx.fill();
-		ctx.closePath();
-		}
-		function drawPaddle() {
-		ctx.beginPath();
-		ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
-		ctx.fillStyle = "#0095DD";
-		ctx.fill();
-		ctx.closePath();
-		}
-		function drawBricks() {
-		for(var c=0; c<brickColumnCount; c++) {
-		for(var r=0; r<brickRowCount; r++) {
-		if(bricks[c][r].status == 1) {
-		var brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
-		var brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
-		bricks[c][r].x = brickX;
-		bricks[c][r].y = brickY;
-		ctx.beginPath();
-		ctx.rect(brickX, brickY, brickWidth, brickHeight);
-		ctx.fillStyle = "#0095DD";
-		ctx.fill();
-		ctx.closePath();
-		}
-		}
-		}
-		}
-		function drawScore() {
-		ctx.font = "16px Arial";
-		ctx.fillStyle = "#0095DD";
-		ctx.fillText("Score: "+score, 8, 20);
-		}
-		function drawLives() {
-		ctx.font = "16px Arial";
-		ctx.fillStyle = "#0095DD";
-		ctx.fillText("Lives: "+lives, canvas.width-65, 20);
-		}
+        var bricks = [];
+        for(var c=0; c<brickColumnCount; c++) {
+            bricks[c] = [];
+            for(var r=0; r<brickRowCount; r++) {
+                bricks[c][r] = { x: 0, y: 0, status: 1 };
+            }
+        }
 
-		function draw() {
-		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		drawBricks();
-		drawBall();
-		drawPaddle();
-		drawScore();
-		drawLives();
-		collisionDetection();
+        document.addEventListener("keydown", keyDownHandler, false);
+        document.addEventListener("keyup", keyUpHandler, false);
+        document.addEventListener("mousemove", mouseMoveHandler, false);
 
-		if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
-		dx = -dx;
-		}
-		if(y + dy < ballRadius) {
-		dy = -dy;
-		}
-		else if(y + dy > canvas.height-ballRadius) {
-		if(x > paddleX && x < paddleX + paddleWidth) {
-		dy = -dy;
-		}
-		else {
-		lives--;
-		if(!lives) {
-		alert("GAME OVER");
-		document.location.reload();
-		}
-		else {
-		x = canvas.width/2;
-		y = canvas.height-30;
-		dx = 2;
-		dy = -2;
-		paddleX = (canvas.width-paddleWidth)/2;
-		}
-		}
-		}
+        function keyDownHandler(e) {
+            if(e.code  == "ArrowRight") {
+                rightPressed = true;
+            }
+            else if(e.code == 'ArrowLeft') {
+                leftPressed = true;
+            }
+        }
+        function keyUpHandler(e) {
+            if(e.code == 'ArrowRight') {
+                rightPressed = false;
+            }
+            else if(e.code == 'ArrowLeft') {
+                leftPressed = false;
+            }
+        }
+        function mouseMoveHandler(e) {
+            var relativeX = e.clientX - canvas.offsetLeft;
+            if(relativeX > 0 && relativeX < canvas.width) {
+                paddleX = relativeX - paddleWidth/2;
+            }
+        }
+        function collisionDetection() {
+            for(var c=0; c<brickColumnCount; c++) {
+                for(var r=0; r<brickRowCount; r++) {
+                    var b = bricks[c][r];
+                    if(b.status == 1) {
+                        if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                            dy = -dy;
+                            b.status = 0;
+                            score++;
+                            if(score == brickRowCount*brickColumnCount) {
+                                alert("YOU WIN, CONGRATS!");
+                                document.location.reload();
+                            }
+                        }
+                    }
+                }
+            }
+        }
 
-		if(rightPressed && paddleX < canvas.width-paddleWidth) {
-		paddleX += 7;
-		}
-		else if(leftPressed && paddleX > 0) {
-		paddleX -= 7;
-		}
+        function drawBall() {
+            ctx.beginPath();
+            ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+        function drawPaddle() {
+            ctx.beginPath();
+            ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+            ctx.fillStyle = "#0095DD";
+            ctx.fill();
+            ctx.closePath();
+        }
+        function drawBricks() {
+            for(var c=0; c<brickColumnCount; c++) {
+                for(var r=0; r<brickRowCount; r++) {
+                    if(bricks[c][r].status == 1) {
+                        var brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
+                        var brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
+                        bricks[c][r].x = brickX;
+                        bricks[c][r].y = brickY;
+                        ctx.beginPath();
+                        ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                        ctx.fillStyle = "#0095DD";
+                        ctx.fill();
+                        ctx.closePath();
+                    }
+                }
+            }
+        }
+        function drawScore() {
+            ctx.font = "16px Arial";
+            ctx.fillStyle = "#0095DD";
+            ctx.fillText("Score: "+score, 8, 20);
+        }
+        function drawLives() {
+            ctx.font = "16px Arial";
+            ctx.fillStyle = "#0095DD";
+            ctx.fillText("Lives: "+lives, canvas.width-65, 20);
+        }
 
-		x += dx;
-		y += dy;
-		requestAnimationFrame(draw);
-		}
+        function draw() {
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            drawBricks();
+            drawBall();
+            drawPaddle();
+            drawScore();
+            drawLives();
+            collisionDetection();
 
-		draw();
-		</script>
+            if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+                dx = -dx;
+            }
+            if(y + dy < ballRadius) {
+                dy = -dy;
+            }
+            else if(y + dy > canvas.height-ballRadius) {
+                if(x > paddleX && x < paddleX + paddleWidth) {
+                    dy = -dy;
+                }
+                else {
+                    lives--;
+                    if(!lives) {
+                        alert("GAME OVER");
+                        document.location.reload();
+                    }
+                    else {
+                        x = canvas.width/2;
+                        y = canvas.height-30;
+                        dx = 2;
+                        dy = -2;
+                        paddleX = (canvas.width-paddleWidth)/2;
+                    }
+                }
+            }
 
-		</body>
-		</html>)rawliteral");
-	});
+            if(rightPressed && paddleX < canvas.width-paddleWidth) {
+                paddleX += 7;
+            }
+            else if(leftPressed && paddleX > 0) {
+                paddleX -= 7;
+            }
 
-	// Send a GET request to <IP>/get?message=<message>
-	server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
-		String message;
-		if (request->hasParam(PARAM_MESSAGE)) {
-			message = request->getParam(PARAM_MESSAGE)->value();
-		} else {
-			message = "No message sent";
-		}
-		request->send(200, "text/plain", "Hello, GET: " + message);
-	});
+            x += dx;
+            y += dy;
+            requestAnimationFrame(draw);
+        }
 
-	// Send a POST request to <IP>/post with a form field message set to <message>
-	server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){
-		String message;
-		if (request->hasParam(PARAM_MESSAGE, true)) {
-			message = request->getParam(PARAM_MESSAGE, true)->value();
-		} else {
-			message = "No message sent";
-		}
-		request->send(200, "text/plain", "Hello, POST: " + message);
-	});
+        draw();
+        </script>
 
-	server.onNotFound(notFound);
+        </body>
+        </html>)rawliteral";
 
-	server.begin();
+        request->send(200, "text/html", html);
+    });
+
+    server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+        updateConnectedIPs(request);
+
+        String message;
+        if (request->hasParam(PARAM_MESSAGE)) {
+            message = request->getParam(PARAM_MESSAGE)->value();
+        } else {
+            message = "No message sent";
+        }
+        request->send(200, "text/plain", "Hello, GET: " + message);
+    });
+
+    server.on("/post", HTTP_POST, [](AsyncWebServerRequest *request){
+        updateConnectedIPs(request);
+
+        String message;
+        if (request->hasParam(PARAM_MESSAGE, true)) {
+            message = request->getParam(PARAM_MESSAGE, true)->value();
+        } else {
+            message = "No message sent";
+        }
+        request->send(200, "text/plain", "Hello, POST: " + message);
+    });
+
+    server.onNotFound(notFound);
+
+    server.begin();
 }
 
 void loop() {
